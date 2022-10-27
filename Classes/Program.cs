a67f97a343ae11ad3f23808ae10db8cc69ef10c3
kyle jenne <strongagent1@gmail.com>
2022-10-27 15:33:41 -0400

    classes exercise 1 KJ

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Classes
+﻿using System.Xml.Schema;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "GMC";
+            myCar.Model = "Yukon";
+            myCar.Year = 2021;
+
+            var chevy = new Car()
+            {
+                Make = "Chevy",
+                Model = "Tahoe",
+                Year = 2019
+            };
+            var ford = new Car()
+            {
+                Make = "Ford",
+                Model = "Bronco",
+                Year = 2022
+
+            };
+            var carList = new List<Car>() { myCar, ford, chevy };
+            
+            foreach(var car in carList)
+            {
+                Console.WriteLine(car.Year);
+                Console.WriteLine(car.Make);
+                Console.WriteLine(car.Model);
+                
+            }
         }
     }
 }
